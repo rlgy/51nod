4a2eb8934587c3512116858f0fd3680c1f5b3adb
rlgy <1032998920@qq.com>
2017-12-15 15:25:55 +0800

    快速幂

---
+++ 1046.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rlgy
+ * Date: 2017-12-14
+ * Time: 16:10
+ */
+
+list($a, $b, $c) = fscanf(STDIN, "%d %d %d");
+
+/**
+结合律
+((a+b) mod p + c)mod p = (a + (b+c) mod p) mod p
+((a*b) mod p * c)mod p = (a * (b*c) mod p) mod p
+交换律
+(a + b) mod p = (b+a) mod p
+(a × b) mod p = (b × a) mod p
+分配律
+((a +b)mod p × c) mod p = ((a × c) mod p + (b × c) mod p) mod p
+(a×b) mod c=(a mod c * b mod c) mod c
+(a+b) mod c=(a mod c+ b mod c) mod c
+(a-b) mod c=(a mod c- b mod c) mod c
+ */
+function get_data($a, $b, $c)
+{
+    $a = $a % $c;
+
+    $ans = 1;
+    while ($b != 0) {
+        if ($b & 1) {
+            $ans = $ans * $a % $c;
+        }
+        $a = $a * $a % $c;
+        $b >>= 1;
+    }
+    return $ans;
+}
+
+print get_data($a, $b, $c);
