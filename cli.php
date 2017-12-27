3b6963a1076eadfd00edbfe7a011ae6154428c2c
rlgy <1032998920@qq.com>
2017-12-27 16:58:14 +0800

    1134 最长递增子序列

--- cli.php
+++ cli.php
@@ -6,36 +6,33 @@
  * Time: 17:49
  */
 
-function get_result($n, $numbers, $q, $querys)
+function get_result($n, $m)
 {
-    $result = [];
-    $sum[0] = 0;
-    for ($i = 1; $i <= $n; $i++) {
-        $sum[$i] = $sum[$i - 1] + $numbers[$i - 1];
+    //f[i,j]=f[i+1][j]+f[i][j+1];
+    /**
+     * 0 0 0
+     * 0 0 0
+     */
+    $w[0][0] = 1;
+
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = 0; $j < $m; $j++) {
+            if ($i == 0) {
+                $w[0][$j] = 1;
+            }
+            if ($j == 0) {
+                $w[$i][0] = 1;
+            }
+        }
     }
-    for ($i = 0; $i < $q; $i++) {
-        $result[$i] = $sum[$querys[$i][1] + $querys[$i][0] - 1] - $sum[$querys[$i][0] - 1];
+    for ($i = 1; $i < $n; $i++) {
+        for ($j = 1; $j < $m; $j++) {
+            $w[$i][$j] = ($w[$i - 1][$j] + $w[$i][$j - 1]) % (100000000 + 7);
+        }
     }
-    return $result;
+    return $w[$n - 1][$m - 1];
 }
 
-$result  = [];
-$numbers = [];
-$querys  = [];
-
-list($n) = fscanf(STDIN, "%d");
-$m = $n;
-while ($n) {
-    list($numbers[]) = fscanf(STDIN, "%d");
-    $n--;
-}
-list($n) = fscanf(STDIN, "%d");
-$q = $n;
-while ($n) {
-    $querys[] = fscanf(STDIN, "%d %d");
-    $n--;
-}
-$result = get_result($m, $numbers, $q, $querys);
-foreach ($result as $v) {
-    print $v . "\n";
-}
+//list($n, $m) = fscanf(STDIN, "%d %d");
+//print get_result($n, $m);
+print pow(10, 9) + 7;
